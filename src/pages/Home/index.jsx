59bb63ad20606e36fd978a2312fdb574580a33ac
leonardo <leonardocave934@gmail.com>
2023-06-19 17:33:53 -0300

    ReactJs fundamentos

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,22 @@
 import './styles.css'
 
+import { Card } from '../../components/Card'
+
+function handleNameChange(name){
+  console.log(name)
+} 
+
 export function Home() {
   return (
-    <>
+    <div className='container'>
       <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..." />
+      
+      <input type="text" placeholder="Digite o nome..."  onChange={e => handleNameChange(e.target.value)}/>
       <button type="button">Adicionar</button>
-    </>
+
+      <Card name="Leonardo" time="21:26:25"/>
+      <Card name="Nicoli" time="03:28:49"/>
+
+    </div >
   );
 }
