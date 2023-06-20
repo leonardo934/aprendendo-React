1cd41da81225d88ea178c2b9d065220e14932cc7
leonardo <leonardocave934@gmail.com>
2023-06-20 17:52:59 -0300

    fazendo aparecer os nomes adicionados usando api do github para pegar o avatar e o name

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,53 @@
+import React, { useState, useEffect } from 'react';
 import './styles.css'
 
 import { Card } from '../../components/Card'
 
-function handleNameChange(name){
-  console.log(name)
-} 
-
 export function Home() {
-  return (
-    <div className='container'>
-      <h1>Lista de Presença</h1>
-      
-      <input type="text" placeholder="Digite o nome..."  onChange={e => handleNameChange(e.target.value)}/>
-      <button type="button">Adicionar</button>
+const [studentName, setStudentName] = useState();
+const [students,setStudents] = useState([]);
+const [user, setUser] = useState({ name:'', avatar: ''})
 
-      <Card name="Leonardo" time="21:26:25"/>
-      <Card name="Nicoli" time="03:28:49"/>
+function handleAddStudent(){
+  const newStudent = {
+    name: studentName,
+    time: new Date().toLocaleTimeString("pt-br" , {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    })
+  };
+
+  setStudents(prevState => [...prevState, newStudent])
+}
+
+
+useEffect(() => {
+ fetch('https://api.github.com/users/leonardo934').then(Response => Response.json()).then(data => {
+  setUser({
+    name: data.name,
+    avatar: data.avatar_url,
+  })
+ })
+},[])
+
+ return (
+    <div className='container'>
+      <header>
+      <h1>Lista de Presença</h1>
+      <div>
+    <strong>{user.name}</strong>
+    <img src={user.avatar} alt="Foto de perfil" />
+      </div>
+
+
+</header>
+
+
+      <input type="text" placeholder="Digite o nome..."  onChange={e => setStudentName(e.target.value)}/>
+      <button type="button" onClick={handleAddStudent}>Adicionar</button>
+
+        {students.map(student => (<Card key={student.time} name={student.name} time={student.time}/>)) }
 
     </div >
   );
